225c982e1ac57060d7936187126c871759be30d9
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-07-17 19:51:16 -0600

    merge sort

--- ServerSide/src/main/java/util/algorithms/Sort.java
+++ ServerSide/src/main/java/util/algorithms/Sort.java
@@ -57,4 +57,40 @@ public class Sort {
             arr[i] = temp;
         }
     }
+
+    public void sort(int[] arr, int[] temp, int low, int high) {
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+            sort(arr, temp, low, mid);
+            sort(arr, temp, mid + 1, high);
+            merge(arr, temp, low, mid, high);
+        }
+    }
+
+
+    //merges two sorteda arrays.
+    public void merge(int[] arr, int[] temp, int low, int mid, int high) {
+
+        for(int i = low; i < high; i++) {
+            temp[i] = arr[i];
+        }
+        int i = low; int j = mid + 1; int k = low;
+
+        while(i <= mid && j <= high){
+            if(temp[i] < temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                temp[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i <= mid){
+            arr[k] = temp[i];
+            i++; k++;
+        }
+
+    }
 }
