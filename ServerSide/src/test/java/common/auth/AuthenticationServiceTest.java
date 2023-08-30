48fe19c8192f2bae86c0118e9b3eda81b0f9c664
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-08-30 14:50:26 -0600

    file changes to next app

--- ServerSide/src/test/java/common/auth/AuthenticationServiceTest.java
+++ ServerSide/src/test/java/common/auth/AuthenticationServiceTest.java
@@ -1,2 +1,58 @@
-package common.auth;public class AuthenticationServiceTest {
+package common.auth;
+
+import common.User.UserRepository;
+import common.config.JwtService;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AuthenticationServiceTest {
+
+    @Mock
+    private UserRepository _userRepository;
+
+    @Mock
+    private PasswordEncoder _passwordEncoder;
+
+    @Mock
+    private JwtService _jwtService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testRegister() {
+        RegisterRequest request = new RegisterRequest("Borbley", "Bonson", "something@gmail.com", "password");
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
