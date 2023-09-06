f3c7c8dfca957807f94403b56b9db4ce34d3f4b9
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-09-06 15:59:25 -0600

    basic microphone visualizer

--- ServerSide/src/test/java/common/auth/AuthenticationServiceTest.java
+++ ServerSide/src/test/java/common/auth/AuthenticationServiceTest.java
@@ -1,33 +1,72 @@
 package common.auth;
 
+import common.User.Role;
 import common.User.UserRepository;
 import common.config.JwtService;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import common.User.User;
+
+
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
-    @Mock
     private UserRepository _userRepository;
-
-    @Mock
     private PasswordEncoder _passwordEncoder;
-
-    @Mock
     private JwtService _jwtService;
+    private AuthenticationManager _authenticationManager;
+    private AuthenticationService _authenticationService;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        _userRepository = Mockito.mock(UserRepository.class);
+        _passwordEncoder = Mockito.mock(PasswordEncoder.class);
+        _jwtService = Mockito.mock(JwtService.class);
+        _authenticationManager = Mockito.mock(AuthenticationManager.class);
+
+        _authenticationService = new AuthenticationService(
+                _userRepository,
+                _passwordEncoder,
+                _jwtService,
+                _authenticationManager
+        );
     }
 
     @Test
     public void testRegister() {
         RegisterRequest request = new RegisterRequest("Borbley", "Bonson", "something@gmail.com", "password");
-        
+        when(_passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+
+        AuthenticationResponse response = _authenticationService.register(request);
+
+        assertEquals("encodedPassword", response.getToken());
+    }
+
+    @Test
+    public void testAuthenticate() {
+        AuthenticationRequest request = new AuthenticationRequest("borbleybonson@bonsonsbagels.com", "password");
+        User user = User
+                .builder()
+                .firstname("Borbley")
+                .lastname("Bonson")
+                .email("borbleybonson@bonsonsbagels.com")
+                .password("encodedPassword")
+                .role(Role.USER)
+                .build();
+
+        when(_userRepository.findByEmail(request.getEmail())).thenReturn(java.util.Optional.of(user));
+        when(_jwtService.generateToken(user)).thenReturn("jwtToken");
+
+        AuthenticationResponse response = _authenticationService.authenticate(request);
+
+        assertEquals("jwtToken", response.getToken());
     }
 }
 
--- reactApp/src/Visualizer/Visualizer.js
+++ reactApp/src/Visualizer/Visualizer.js
@@ -2,6 +2,57 @@ import React, { useRef, useEffect } from "react";
 import style from './visualizer.css'
 import Layout from "../Header/Layout";
 
+class Bar {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    update(micInput) {
+        this.height = micInput * 1000;
+    }
+    draw(context) {
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Microphone {
+    constructor() {
+        this.initialized = false;
+        navigator.mediaDevices.getUserMedia({audio:true})
+        .then((stream) => {
+            this.audioContext = new AudioContext();
+            this.microphone = this.audioContext.createMediaStreamSource(stream)
+            this.analyser = this.audioContext.createAnalyser();
+            this.analyser.fftSize = 512;
+            const bufferLength = this.analyser.frequencyBinCount;
+            this.dataArray = new Uint8Array(bufferLength);
+            this.microphone.connect(this.analyser)
+            this.initialized = true;
+        }).catch(function(err){
+            alert(err);
+        });
+    }
+    getSamples() {
+        this.analyser.getByteTimeDomainData(this.dataArray);
+        let normSamples = [...this.dataArray].map(e => e/128 - 1);
+        return normSamples;
+    }
+    getVolume() {
+        this.analyser.getByteTimeDomainData(this.dataArray);
+        let normSamples = [...this.dataArray].map(e => e/128 - 1);
+        let sum = 0;
+        for(let i = 0; i < normSamples.length; i++) {
+            sum += normSamples[i] * normSamples[i];
+        }
+        let volume = Math.sqrt(sum / normSamples.length);
+        return volume;
+    }
+}
+
 const Visualizer = () => {
     const canvasRef = useRef(null);
 
@@ -10,28 +61,33 @@ const Visualizer = () => {
         if (!canvas) {
             return; // Canvas element not yet available
         }
-        console.log("hello");
         const ctx = canvas.getContext('2d');
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     
-        class Bar {
-            constructor() {
-    
-            }
-            update() {
-     
-            }
-            draw() {
-    
+        const microphone = new Microphone();
+
+        let bars = [];
+        let barWidth = canvas.width/256;
+        const createBars = () => {
+            for(let i = 0; i < 256; i++) {
+                let color = 'hsl(' + i + ', 100%, 50%)'
+                bars.push(new Bar(i * barWidth, canvas.height/2, 1, 20, color));
             }
         }
+        createBars();
     
         const animate = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            //generate audio samples from microphone
-            //animate bars based on microphone data
-            console.log('animate');
+            if(microphone.initialized) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //generate audio samples from microphone
+                const samples = microphone.getSamples();
+                //animate bars based on microphone data
+                bars.forEach((bar, i) => {
+                    bar.update(samples[i]);
+                    bar.draw(ctx);
+                });
+            }
             requestAnimationFrame(animate);
         }
         animate();
@@ -42,13 +98,12 @@ const Visualizer = () => {
         <Layout>
             <div className={style.main}>
                 <div className={style.body}>
-                    <canvas className={style.canvas}>
+                    <canvas ref={canvasRef} className={style.canvas}>
                         
                     </canvas>
                 </div>
             </div>
         </Layout>
-
     )
 }
 
